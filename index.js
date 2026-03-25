a790968ec4fad65a2b87a3f1b5856f1d080233ee
Vishnu Vardhan <vishnuvardhan25072003@gmail.com>
2026-03-25 17:01:16 +0530

    Added dynamic port for deployment

--- index.js
+++ index.js
@@ -96,6 +96,8 @@ app.delete('/users/:id', (req, res) => {
 });
 
 /* SERVER */
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
